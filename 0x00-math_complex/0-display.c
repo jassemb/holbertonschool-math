4eef800abccc514a314134b9258743896a3cdc28
jassemb <benalijassem201@gmail.com>
2021-07-30 13:23:36 -0700

    display

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -7,13 +7,26 @@
 
 void display_complex_number(complex c)
 {
-	if (c.im > 0)
-		printf("%0.f + %0.fi\n", c.re, c.im);
-	else if (c.im < 0)
+	if (c.re == 0)
 	{
-		c.im = -c.im;
-		 printf("%0.f - %0.fi\n", c.re, c.im);
+		if (c.im < 0)
+			printf("- %.9gi\n", c.im * (-1));
+		else if (c.im == 0)
+			printf("0\n");
+		else
+			printf("%.9g\n", c.im);
 	}
 	else
-		  printf("%0.f\n", c.re);
+	{
+		if (c.im < -1)
+			printf("%.9g - %.9gi\n", c.re, c.im * (-1));
+		else if (c.im == -1)
+			printf("%.9g - i\n", c.re);
+		else if (c.im == 0)
+			printf("%.9g\n", c.re);
+		else if (c.im == 1)
+			printf("%.9g + i\n", c.re);
+		else
+			printf("%.9g + %.9gi\n", c.re, c.im);
+	}
 }
