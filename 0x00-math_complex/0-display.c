4687762a54b5b4240eb222bce94807a7790365b5
jassemb <benalijassem201@gmail.com>
2021-07-30 12:36:41 -0700

    display

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,9 +6,13 @@
 
 void display_complex_number(complex c)
 {
-	if (c.im == 0)
-		printf("%0.f\n", c.re);
-	else
+	if (c.im > 0)
 		printf("%0.f + %0.fi\n", c.re, c.im);
-
+	else if (c.im < 0)
+	{
+		c.im = -c.im;
+		 printf("%0.f - %0.fi\n", c.re, c.im);
+	}
+	else
+		  printf("%0.f\n", c.re);
 }
